29450b705dc3abb0ce262b8d25ae6fc9f6716f74
Sara Connelly <sara.k.connelly@gmail.com>
2023-06-07 20:12:24 -0600

    Add code challenge responses

---
+++ codeChallenges/mild.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program that defines a variable that stores a Number. 
+// The program should print out the String "even" if the Number is even, 
+// and the String "odd" if the Number is odd. Hint: You may need to do some 
+// extra research on the remainder operator (%) to solve this challenge.
+
+var num = 14 
+if (num%2 === 1) {
+    console.log("odd");
+} else {
+    console.log("even");
+}
